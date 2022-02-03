1afd0e71e37c95a42a18e5dea973914dba098c03
Kethly <57457270+Kethly@users.noreply.github.com>
2022-02-03 10:10:40 -0500

    Update db.js

--- db.js
+++ db.js
@@ -1,3 +1,6 @@
-test = await fetch(f"https://sheets.googleapis.com/v4/spreadsheets/1XBd9iby84O-jNv0Wuvzewtm9ZKyEn87NpFmSIy-8HVA/values/A1?key={process.env.TOKEN}")
-resp = await test.json()
-console.log(resp)
+async function receivedata(){
+test = await fetch(f"https://sheets.googleapis.com/v4/spreadsheets/1XBd9iby84O-jNv0Wuvzewtm9ZKyEn87NpFmSIy-8HVA/values/A1?key={process.env.TOKEN}");
+resp = await test.json();
+console.log(resp);
+}
+receivedata();
