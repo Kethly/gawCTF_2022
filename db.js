c1eabfca95cf63abf9d662a2fcf68c996c6d1256
Kethly <57457270+Kethly@users.noreply.github.com>
2022-02-03 10:08:27 -0500

    Create db.js

---
+++ db.js
@@ -0,0 +1,3 @@
+test = await fetch(f"https://sheets.googleapis.com/v4/spreadsheets/1XBd9iby84O-jNv0Wuvzewtm9ZKyEn87NpFmSIy-8HVA/values/A1?key={process.env.TOKEN}")
+resp = await test.json()
+console.log(resp)
