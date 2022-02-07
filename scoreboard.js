5aef4e488d2d36a1465788fe3ef0baeaef3792b3
Kethly <57457270+Kethly@users.noreply.github.com>
2022-02-07 12:50:56 -0500

    Update scoreboard.js

--- scoreboard.js
+++ scoreboard.js
@@ -4,6 +4,7 @@ async function getScoreboard(){
   var display = "";
   for(var i = 0; i < Object.keys(responses).length; i++){
     display += JSON.stringify(responses[i]) + "\n";
+    console.log(JSON.stringify(responses[i]) + "\n");
   }
   document.getElementById("scoreboard").innerHTML = display;
 }
