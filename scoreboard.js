8d5ede638e4e34484d40fdb19d89ab63ad617577
Kethly <57457270+Kethly@users.noreply.github.com>
2022-02-07 12:36:33 -0500

    Create scoreboard.js

---
+++ scoreboard.js
@@ -0,0 +1,9 @@
+async function getScoreboard(){
+  var publicdata = await fetch(https://docs.google.com/spreadsheets/d/1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms/edit);
+  var responses = await publicdata.json();
+  var display = "";
+  for(var i = 0; i < Object.keys(responses).length; i++){
+    display += JSON.stringify(responses[i]) + "\n";
+  }
+  document.getElementById("scoreboard").innerHTML = display;
+}
