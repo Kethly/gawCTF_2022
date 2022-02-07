f4e41b7ee58120a7a29fdade3abf742c46a62ed4
Kethly <57457270+Kethly@users.noreply.github.com>
2022-02-07 12:38:50 -0500

    Update scoreboard.js

--- scoreboard.js
+++ scoreboard.js
@@ -1,5 +1,5 @@
 async function getScoreboard(){
-  var publicdata = await fetch(https://docs.google.com/spreadsheets/d/1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms/edit);
+  var publicdata = await fetch("https://docs.google.com/spreadsheets/d/1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms/edit");
   var responses = await publicdata.json();
   var display = "";
   for(var i = 0; i < Object.keys(responses).length; i++){
