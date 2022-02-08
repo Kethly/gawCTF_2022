03d59fcf57019bd688b7b813b9c8983e068f793e
Kethly <57457270+Kethly@users.noreply.github.com>
2022-02-08 01:26:42 -0500

    Update upload.js

--- functions/upload.js
+++ functions/upload.js
@@ -5,7 +5,7 @@ const { google } = require("googleapis");
 async function upload(){
   var SCOPES = "https://www.googleapis.com/auth/spreadsheets";
   var path = "1XBd9iby84O-jNv0Wuvzewtm9ZKyEn87NpFmSIy-8HVA";
-  var read = await fetch(`https://sheets.googleapis.com/v4/spreadsheets/${path}/?key=${token}`;
+  var read = await fetch(`https://sheets.googleapis.com/v4/spreadsheets/${path}/?key=${token}`);
   var values = await read.response();
   console.log(values);
 }
