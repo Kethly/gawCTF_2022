2e3338a16da4975d4fe2bcf2879ba7205cb5d193
Kethly <57457270+Kethly@users.noreply.github.com>
2022-02-08 01:31:35 -0500

    Update upload.js

--- functions/upload.js
+++ functions/upload.js
@@ -2,11 +2,10 @@ var token = process.env.TOKEN;
 const fetch = require('node-fetch');
 //googleapis
 const { google } = require("googleapis");
-async function upload(){
+exports.handler = async (event, context) => {
   var SCOPES = "https://www.googleapis.com/auth/spreadsheets";
   var path = "1XBd9iby84O-jNv0Wuvzewtm9ZKyEn87NpFmSIy-8HVA";
   var read = await fetch(`https://sheets.googleapis.com/v4/spreadsheets/${path}/?key=${token}`);
   var values = await read.response();
-  console.log(values);
-}
-upload();
+return { statusCode: 200, body: JSON.stringify(values), };
+};
