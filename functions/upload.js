5adeabca4a9a614e195a8f8030c207a323c08f2c
Kethly <57457270+Kethly@users.noreply.github.com>
2022-02-08 01:57:18 -0500

    Update upload.js

--- functions/upload.js
+++ functions/upload.js
@@ -4,7 +4,7 @@ const fetch = require('node-fetch');
 const { google } = require("googleapis");
 exports.handler = async (event, context) => {
   var SCOPES = "https://www.googleapis.com/auth/spreadsheets";
-  var path = "1XBd9iby84O-jNv0Wuvzewtm9ZKyEn87NpFmSIy-8HVA";
+  var path = "15VYcKvXIXfGaLPMzBojLMOnt-ajWn505lNzWwTmNoD0";
   var read = await fetch(`https://sheets.googleapis.com/v4/spreadsheets/${path}/?key=${token}`);
   var values = await read.json();
 return { statusCode: 200, body: JSON.stringify(values), };
