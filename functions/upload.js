4f0ed81f823c5fd72bb0cc01288968f51e44b18c
Kethly <57457270+Kethly@users.noreply.github.com>
2022-02-08 01:34:34 -0500

    Update upload.js

--- functions/upload.js
+++ functions/upload.js
@@ -6,6 +6,6 @@ exports.handler = async (event, context) => {
   var SCOPES = "https://www.googleapis.com/auth/spreadsheets";
   var path = "1XBd9iby84O-jNv0Wuvzewtm9ZKyEn87NpFmSIy-8HVA";
   var read = await fetch(`https://sheets.googleapis.com/v4/spreadsheets/${path}/?key=${token}`);
-  var values = await read.response();
+  var values = await read.json();
 return { statusCode: 200, body: JSON.stringify(values), };
 };
