be9fa322ae540b5c3412b94833b2022000cf8dbf
Kethly <57457270+Kethly@users.noreply.github.com>
2022-02-09 11:39:40 -0500

    Update upload.js

--- functions/upload.js
+++ functions/upload.js
@@ -31,8 +31,9 @@ sheets.spreadsheets.values.get({
        console.log('The API returned an error: ' + err);
    } else {
        console.log('Movie list from Google Sheets:');
+     test = await responpse.values;
        for (let row of response.values) {
-           test.push('Title [%s]\t\tRating [%s]' + " " + row[0] + " "+ row[1]);
+           //test.push('Title [%s]\t\tRating [%s]' + " " + row[0] + " "+ row[1]);
        }
    }
 });
